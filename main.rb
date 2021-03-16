ff03ea6d7c2cb500e9d8cb4c7eddd146ec711b59
shitika <kuroko9019@gmail.com>
2021-03-16 11:45:51 +0900

    キャラクターの表示

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 require 'dxruby'
 
+require_relative './src/player/player'
+
+player = Player_base.new
+
 Window.width = 800
 Window.height = 600
 
 Window.loop do
+  player.draw
 end
---
+++ src/player/player.rb
@@ -0,0 +1,14 @@
+#プレイヤーの呼び出しと機能まとめ
+class Player_base
+
+  def initialize
+    @image = Image.load("./src/player/images/player.png")
+    @image.set_color_key(C_WHITE)
+    @x = 40
+    @y = 40
+  end
+
+  def draw
+    Window.draw(@x, @y, @image)
+  end
+end
