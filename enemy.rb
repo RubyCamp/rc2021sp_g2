ba6e8ea55daca0f4e86a876542653003a1da30b0
atarubift <atarubift@gmail.com>
2021-03-16 15:49:37 +0900

    Add enemy move

---
+++ enemy.rb
@@ -0,0 +1,40 @@
+class Enemy < Sprite
+  def initialize(x, y)
+    @speed = 5
+    image = Image.load("images/enemy.png")
+    image.set_color_key(C_WHITE)
+    super(x, y, image)
+  end
+
+  def update
+    self.x -= @speed
+
+    if self.x + 45 <= 0
+      self.vanish
+    end
+  end
+end
+
+class Enemies
+  MAX_ENEMY = 5
+
+  def initialize
+    @enemies = []
+    @enemyPlace = [355, 455]
+  end
+
+  def update
+    Sprite.update(@enemies)
+    Sprite.clean(@enemies)
+
+    (MAX_ENEMY - @enemies.size).times do
+      if rand(1..100) > 99
+        @enemies << Enemy.new(800, @enemyPlace[rand(0..1)])
+      end
+    end
+  end
+
+  def draw
+    Sprite.draw(@enemies)
+  end
+end
--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
 require 'dxruby'
 
+require_relative 'enemy'
+
 Window.width = 800
 Window.height = 600
 
+enemies = Enemies.new
+
 Window.loop do
+  enemies.update
+  enemies.draw
 end
