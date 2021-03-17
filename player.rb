11ec2ccc53d532522f7d2594ea9a5db797f1418f
shitika <kuroko9019@gmail.com>
2021-03-18 00:48:42 +0900

    Addition collisin

--- player.rb
+++ player.rb
@@ -8,51 +8,53 @@ class Player < Sprite
     @x = 20
     @y = 490
     @dy = 0#y座標の増加量
-    @speed = 5
+    @speed     = 5
     #キャラクターのSpriteに値を渡す
     self.x = @x
     self.y = @y
     self.image = @image
 
     @jump_flag = false
-    @under = self.y + 81#足元をY座標に
+    @under = self.y + @image.height#足元をY座標に
   end
 
   def jump
     if Input.key_push?(K_UP) and @jump_flag
-      @dy = - 18
+      @dy = - 16
       @jump_flag = false
     end
     @under += @dy
-    self.y = @under -81
+    self.y = @under - @image.height
     @dy += 1 #重力的な
   end
 
   def update
-    @speed = 5
+    @speed_mag = 1.5
     if Input.key_down?(K_LEFT) && self.x > 100
-      self.x -= @speed
-    elsif Input.key_down?(K_RIGHT) && self.x + 47 < 800
+      self.x -= @speed * @speed_mag
+    elsif Input.key_down?(K_RIGHT) && self.x + @image.width < 800
       self.x += @speed
     end
   end
 
   #衝突判定が来たときにy座標を変更
   def shot_way(d)
-    way_y = d.y
+    way_y  = d.y
     @under = way_y
     @dy = 0
     @jump_flag = true
-    @under
   end
   def shot_obs(d)
     obs_x = d.x
     obs_y = d.y
-    obj_id = d.object_id
     if @under - @dy < obs_y
       @under = obs_y
       @dy = 0
       @jump_flag = true
+    elsif obs_x > self.x
+      self.x = obs_x - @image.width
+    elsif obs_x + 100 < self.x + 3
+        self.x += @speed
     end
 
   end
