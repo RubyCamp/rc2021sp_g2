68f0a6582cf8d09e65144761a525f0216458a542
unknown <b1700365@gl.aiu.ac.jp>
2021-03-18 13:33:12 +0900

    add cloud and judge collision

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require_relative 'way'
 require_relative 'enemy'
 require_relative './src/player'
 require_relative 'wall'
+require_relative 'cloud'
 
 Window.width = 800
 Window.height = 600
@@ -12,6 +13,7 @@ ways = Ways.new
 enemies = Enemies.new
 plyr = Player.new
 wall = Wall.new
+clouds = Clouds.new
 
 Window.loop do 
   ways.update
@@ -23,4 +25,12 @@ Window.loop do
   plyr.update
   plyr.jump
   plyr.draw
+  clouds.update(plyr)
+  clouds.draw
+  # plyr.check(cloud.getSprite)
+  # => [] 配列の長さ（要素数）が0
+  # => [sprite1, sprite2].size 配列の長さ（要素数）が2
+  # if plyr.check(cloud.getSprite).size != 0
+  #   cloud.shot
+  # end
 end
--- wall.rb
+++ wall.rb
@@ -9,14 +9,15 @@ class Wall < Sprite
     end
     
     def update
-        Window.draw(0,0,@image) 
-        Window.draw(0,60,@image) 
-        Window.draw(0,120,@image) 
-        Window.draw(0,180,@image) 
-        Window.draw(0,240,@image) 
-        Window.draw(0,300,@image) 
-        Window.draw(0,360,@image) 
-        Window.draw(0,420,@image) 
-        Window.draw(0,480,@image) 
+        10.times { |i| Window.draw(0,60*i,@image) }
+        #Window.draw(0,0,@image) 
+        #Window.draw(0,60,@image) 
+        #Window.draw(0,120,@image) 
+        #Window.draw(0,180,@image) 
+        #Window.draw(0,240,@image) 
+        #Window.draw(0,300,@image) 
+        #Window.draw(0,360,@image) 
+        #Window.draw(0,420,@image) 
+        #Window.draw(0,480,@image) 
     end
 end
